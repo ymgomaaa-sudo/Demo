20d114cf6f68b20dfc8a23cea0be722f01a41e07
ALBORAQ <ALBORAQ@DESKTOP-59J143T>
2026-09-09 14:28:45 +0300

    Question1-2  and  Question2-1 and Question2-2

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,13 +10,36 @@
             book1.Pages = 300;
             object x = book1;
             Console.WriteLine(x);
+            #region Question 2-2
+            Console.WriteLine(book1.ToString());
+            Console.WriteLine(book1.Equals(book1));
+            Console.WriteLine(book1.GetHashCode()); 
+            Console.WriteLine(book1.GetType());
+            #endregion
+
+            #endregion
+            #region question 2-3
+            Book book2 = new Book();
+            book2.Title = "book2 Name";
+            book2.Pages = 350;
+            object y = book2;
+            Console.WriteLine(y);
+            Console.WriteLine(book1.Equals(book2));
             #endregion
         }
+
         class Book
-        {
+        { 
+            #region Question 1-2
             public string Title;
             public int Pages;
-            
+            #endregion
+            #region Question 2-1
+            public override string ToString()
+            {
+                return $"Title :{Title}, Pages :{Pages}";
+            }
+            #endregion
         }
     }
 }
